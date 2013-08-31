10c5a2c518f1f6c6a643c54050389231c3e9a9c5
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2013-08-30 19:02:39 -0500

    Move producer/consumer tests into pipe tests

--- Sherlock.Tests/Generic/BoundedBufferTests.cs
+++ Sherlock.Tests/Generic/BoundedBufferTests.cs
@@ -9,116 +9,15 @@ using Sherlock.Collections.Generic;
 
 namespace Sherlock.Collections.Generic.Tests
 {
-
     [TestFixture]
     class BoundedBufferTests
     {
         private BoundedBuffer<int> boundedBuffer;
-        private ManualResetEvent doneEvent;
-        private Action producer;
-        private Action consumer;
 
         [SetUp]
         public void SetUp()
         {
-            doneEvent = new ManualResetEvent(false);
             boundedBuffer = new BoundedBuffer<int>(2);
-
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            boundedBuffer.Dispose();
-            doneEvent.Dispose();
-        }
-
-        [Test]
-        public void Test_SlowConsumer()
-        {
-            // Assert
-            int result = 0;
-            producer = () =>
-                {
-                    for (int i = 0; i < 10; i++)
-                        boundedBuffer.Put(i);
-                };
-            consumer = () =>
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        int item = boundedBuffer.Take();
-                        result += item;
-                        Thread.Sleep(50);
-                    }
-                    doneEvent.Set();
-                };
-
-            // Act
-            ParallelThread.Invoke(producer, consumer);
-            doneEvent.WaitOne();
-
-            // Assert
-            Assert.AreEqual(45, result);
-        }
-        
-        [Test]
-        public void Test_ManyProducers()
-        {
-            // Assert
-            int result = 0;
-            producer = () =>
-                {
-                    for (int i = 0; i < 10; i++)
-                        boundedBuffer.Put(i);
-                };
-            consumer = () =>
-                {
-                    for (int i = 0; i < 30; i++)
-                    {
-                        int item = boundedBuffer.Take();
-                        result += item;
-                    }
-                    doneEvent.Set();
-                };
-
-            // Act
-            ParallelThread.Invoke(producer, producer, producer, consumer);
-            doneEvent.WaitOne();
-
-            // Assert
-            Assert.AreEqual(45 * 3, result);
-        }
-
-        [Test]
-        public void Test_SlowProducer()
-        {
-            // Assert
-            int result = 0;
-            producer = () =>
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        boundedBuffer.Put(i);
-                        Thread.Sleep(50);
-                    }
-                };
-            consumer = () =>
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        int item = boundedBuffer.Take();
-                        result += item;
-                    }
-                    doneEvent.Set();
-                };
-
-            // Act
-            ParallelThread.Invoke(producer, consumer);
-            doneEvent.WaitOne();
-
-            // Assert
-            Assert.AreEqual(45, result);
         }
 
         [Test]
@@ -214,22 +113,6 @@ namespace Sherlock.Collections.Generic.Tests
 
             // Assert
             Assert.Fail("Failed to raise exception when buffer is disposed.");
-        }
-
-        /// <summary>
-        /// Since Parallel.Invoke does not guarantee the actions will
-        /// run on separate threads, use this helper class. 
-        /// </summary>
-        private static class ParallelThread
-        {
-            public static void Invoke(params Action[] actions)
-            {
-                foreach (var action in actions)
-                {
-                    var thread = new Thread(new ThreadStart(action));
-                    thread.Start();
-                }
-            }
         }
     }
 }
---
+++ Sherlock.Tests/Generic/PipeTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using Sherlock.Collections.Generic;
+
+namespace Sherlock.Tests.Generic
+{
+    [TestFixture]
+    public class PipeTests
+    {
+        private IPipe<int> pipe;
+        private Action producer;
+        private Action consumer;
+        private ManualResetEvent doneEvent;
+
+        [SetUp]
+        public void SetUp()
+        {
+            pipe = Pipe.Open<int>();
+            doneEvent = new ManualResetEvent(false);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            pipe.Reader.Dispose();
+            pipe.Writer.Dispose();
+            doneEvent.Dispose();
+        }
+
+        [Test]
+        public void Test_SlowConsumer()
+        {
+            // Assert
+            int result = 0;
+            producer = () =>
+                {
+                    for (int i = 0; i < 10; i++)
+                        pipe.Writer.Write(i);
+                };
+            consumer = () =>
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int item;
+                        pipe.Reader.Read(out item);
+                        result += item;
+                        Thread.Sleep(50);
+                    }
+                    doneEvent.Set();
+                };
+
+            // Act
+            ParallelThread.Invoke(producer, consumer);
+            doneEvent.WaitOne();
+
+            // Assert
+            Assert.AreEqual(45, result);
+        }
+
+        [Test]
+        public void Test_ManyProducers()
+        {
+            // Assert
+            int result = 0;
+            producer = () =>
+                {
+                    for (int i = 0; i < 10; i++)
+                        pipe.Writer.Write(i);
+                };
+            consumer = () =>
+                {
+                    for (int i = 0; i < 30; i++)
+                    {
+                        int item;
+                        pipe.Reader.Read(out item);
+                        result += item;
+                    }
+                    doneEvent.Set();
+                };
+
+            // Act
+            ParallelThread.Invoke(producer, producer, producer, consumer);
+            doneEvent.WaitOne();
+
+            // Assert
+            Assert.AreEqual(45 * 3, result);
+        }
+
+        [Test]
+        public void Test_SlowProducer()
+        {
+            // Assert
+            int result = 0;
+            producer = () =>
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        pipe.Writer.Write(i);
+                        Thread.Sleep(50);
+                    }
+                };
+            consumer = () =>
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int item;
+                        pipe.Reader.Read(out item);
+                        result += item;
+                    }
+                    doneEvent.Set();
+                };
+
+            // Act
+            ParallelThread.Invoke(producer, consumer);
+            doneEvent.WaitOne();
+
+            // Assert
+            Assert.AreEqual(45, result);
+        }
+
+        /// <summary>
+        /// Since Parallel.Invoke does not guarantee the actions will
+        /// run on separate threads, use this helper class. 
+        /// </summary>
+        private static class ParallelThread
+        {
+            public static void Invoke(params Action[] actions)
+            {
+                foreach (var action in actions)
+                {
+                    var thread = new Thread(new ThreadStart(action));
+                    thread.Start();
+                }
+            }
+        }
+    }
+}
